7d07803179eb0c00f3d7f46ee720cba88e2abb3a
silas <silasfacanha@hotmail.com>
2023-05-01 10:19:52 +0100

    Solve the padding problem on the Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,9 +4,9 @@ import { HashLink as Link } from "react-router-hash-link";
 const Hero = () => {
   return (
     <section className="100vh">
-      <div className="flex justify-center py-8  "> <img className="block h-72" src={frontImage} /></div>
-      <div className="flex flex-col items-center">
-        <h2 className=" font-semibold"> A project to spread Seth Godin's wisdom.</h2>
+      <div className="flex justify-center py-8 "> <img className="block h-72" src={frontImage} /></div>
+      <div className="flex flex-col items-center p-2">
+        <h2 className="font-semibold"> A project to spread Seth Godin's wisdom.</h2>
         <Link smooth to="#wisdom">
           
           <button className=" font-semibold p-4 border-solid border-2 rounded-lg bg-[#ff366a] hover:bg-[#ff366a] border-transparent">
